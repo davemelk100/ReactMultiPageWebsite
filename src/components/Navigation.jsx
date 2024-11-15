2becdb4b785d5bb36c34054b8cd12a4b21a292e0
davemelk100 <davemelk@gmail.com>
2024-11-15 14:09:02 -0500

    updated font

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,56 +4,73 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div className="navigation">
-      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-        <button className="navbar-toggler mb-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/">
-                Home
-                <span className="sr-only">(current)</span>
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/samples">
-                Samples
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rxapp">
-                Prototype
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/patterns">
-                Patterns
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/skills">
-                Skills
-              </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/resumes">
-                  Resume
-                </NavLink>
-              </li>                  
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">
-                Contact
-              </NavLink>
-            </li>
+    <header className="navigation">
+      <div className="row">
+        <div className="col-sm-12 text-center">
+          <h1 className="header-title">David Melkonian</h1>
+          {/* <small>title</small>
+          <br /> */}
+          <span> - </span>
+          <h2 className="header-second-title">User Experience Development and Design Professional</h2>
+        </div>
 
+        <div className="col-sm-12">
+          <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+            <button
+              className="navbar-toggler mb-2"
+              type="button"
+              data-toggle="collapse"
+              data-bs-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    Home
+                    <span className="sr-only">(current)</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/samples">
+                    Samples
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/rxapp">
+                    Prototype
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/patterns">
+                    Patterns
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/skills">
+                    Skills
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/resumes">
+                    Resume
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/contact">
+                    Contact
+                  </NavLink>
+                </li>
 
-
-
-
-
-            {/* <li className="nav-item">
+                {/* <li className="nav-item">
                 <NavLink className="nav-link" to="/scroll">
                   Scroll
                 </NavLink>
@@ -63,11 +80,12 @@ function Navigation() {
                   Blog
                 </NavLink>
               </li> */}
-          </ul>
+              </ul>
+            </div>
+          </nav>
         </div>
-      </nav>
-
-    </div>
+      </div>
+    </header>
   );
 }
 
