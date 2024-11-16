34164f09686076ea1445504067d6d38f753871a7
davemelk100 <davemelk@gmail.com>
2024-11-16 17:36:19 -0500

    update package and remove mobile img

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import profileImage from "./dm-periodic.svg";
-import profileImageSmall from "./dm-periodic-small.svg";
 
 function Background() {  
   return (
     <div>
       <img src={profileImage} alt="Profile" className="img-art-two position-fixed d-none d-sm-none d-md-none d-lg-flex d-xl-flex" />
-      {/* <img src={profileImageSmall} alt="Profile mobile" className="img-art-two d-none d-xs-flex d-sm-flex d-md-none d-lg-none d-xl-none" /> */}
     </div>
 
   );
