e42165c526936c8ac16069edc6ed13c6ebd9bffe
davemelk100 <davemelk@gmail.com>
2024-11-19 12:55:54 -0500

    swapped out image to use border img

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import dmLogo from "./dm-no-border.svg";
+import dmLogo from "./dm-border-svg.svg";
 
 function Navigation() {
   return (
