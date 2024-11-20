7789c1f5184465296085e59648302d65cbcbf290
davemelk100 <davemelk@gmail.com>
2024-11-20 14:03:25 -0500

    mock data for contact in fo

---
+++ src/services/mockData.js
@@ -0,0 +1,8 @@
+// mockData.js
+export const contactData = [
+    { type: "Email", url: "mailto:davemelk@gmail.com" },
+    { type: "LinkedIn", url: "https://www.linkedin.com/in/davemelk/" },
+    { type: "Dribble", url: "https://dribbble.com/davemelk100" },
+    { type: "Portfolio", url: "https://davemelk.com" },
+  ];
+  
