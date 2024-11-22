91998a0b5380839bfd2d0b003db502e509998249
davemelk100 <davemelk@gmail.com>
2024-11-22 17:29:28 -0500

    updated nav and footer pt 3

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import dmLogo from "./dm-smaller.svg";
+// import dmLogo from "./dm-smaller.svg";
 
 function Navigation() {
   return (
